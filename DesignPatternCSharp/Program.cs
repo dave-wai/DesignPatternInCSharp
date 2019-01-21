f44033dd5c783d0bae25052cc28bfe38aeec3572
Dave Wai <tina.dave.wai@gmail.com>
2019-01-21 22:10:33 +1100

    Initial commit

---
+++ DesignPatternCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatternCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
